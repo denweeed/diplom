702e313989e3506c6f1fb76a29335d7f6d7572dc
denweeed <proverkamobi@gmail.com>
2023-04-14 12:40:27 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,10 +1,65 @@
-from typing import Union
+import pymongo
+from pymongo import MongoClient
+from fastapi import FastAPI, Request
+from pydantic.main import BaseModel
+from pydantic import root_validator, ValidationError
+from enum import Enum
+from datetime import date
 
-from fastapi import FastAPI
+app = FastAPI(
+    title="product"
+)
 
-app = FastAPI()
+
+@app.on_event("startup")
+def startup():
+    app.state.custom = 4
+    print("startup")
+
+
+client = MongoClient('localhost', 27017)
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def read_root(request: Request):
+    customfcs = request.app.state.custom
+    return {"Hello": "customfcs"}
+
+
+class ProductType(str, Enum):
+    FOOD = 'food'
+    EVERYDAY_USE = 'everyday_use'
+
+
+class Product(BaseModel):
+    id: int
+    name: str
+    price: float
+    type: ProductType
+    region: str
+    created_at: date
+
+    @root_validator(pre=True)
+    def validate_id(cls, values):
+        if 'id' not in values:
+            values['id'] = len(products) + 1
+        return values
+
+
+products = []  # список для сохранения продуктов
+
+
+# эдитпоин для добавления нового продукта
+@app.post('/products')
+async def add_product(product: Product):
+    try:
+        products.append(product.dict())
+        return {'status': 'success', 'data': product.dict()}, 201
+    except ValidationError as e:
+        return {'status': 'error', 'message': e.errors()}, 400
+
+
+# эдитпоинт для получения списка всех продуктов
+@app.get('/products/all')
+async def get_all_products():
+    return {'status': 'success', 'data': products}, 200
