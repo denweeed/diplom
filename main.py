b73c9f69031c9cc33bc7dd7178ef1144d628492f
denweeed <proverkamobi@gmail.com>
2023-05-18 15:32:34 +0300

    Added editpoint to add new products

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pymongo
 from pymongo import MongoClient
 from fastapi import FastAPI, Request
-from bson import ObjectId
 from src.models import ProductCreate
 from src.settings import MONGO_URI, MONGO_DB
 
@@ -24,31 +23,21 @@ def read_root(request: Request):
     return {"Hello": "customfcs"}
 
 
-products_data = mongo_db.products.find()
-
-
-@app.get('/products/all')
 def get_all_products():
+    # Retrieve all products from the collection and convert the cursor to a list
+    products_data = list(mongo_db.products.find())
     return products_data
 
 
 # Define the endpoint to add a new Product to MongoDB
 @app.post("/products", status_code=201)
 def add_product_to_db(product: ProductCreate):
-    # Generate a new ObjectId for the product
-    product_id = str(ObjectId())
-
     # Create a new document with the product data
     product_data = {
-        'id': int(product_id),
         'name': product.name,
         'price': product.price,
         'type': product.type,
         'region': product.region
     }
-
-    # Insert the new product document into the MongoDB collection
-    mongo_db.products.insert_one(product_data)
-
     # Return the new product as a dictionary
     return product_data
--- src/settings.py
+++ src/settings.py
@@ -6,5 +6,5 @@ env = environs.Env()
 BASE_DIR = Path(__file__).parent
 
 MONGO_URI = env.str("MONGO_URI", "mongodb://localhost:8081")
-MONGO_DB = "inflation"
-PRODUCTS_COLLECTION = "products"
+MONGO_DB = env.str("MONGO_DB", default="inflation")
+PRODUCTS_COLLECTION = env.str("PRODUCTS_COLLECTION", default="products")
