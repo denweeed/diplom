1437a90b966e7c2094c87dd94162a70938b239af
denweeed <proverkamobi@gmail.com>
2023-05-18 18:19:59 +0300

    Added editpoint to add new products

--- test/test_add_products.py
+++ test/test_add_products.py
@@ -3,20 +3,56 @@ from fastapi.testclient import TestClient
 from main import app
 from src.models import ProductCreate
 
-client = TestClient(app)
-
 
 @pytest.fixture
-def product_data():
-    return {
+def client():
+    return TestClient(app)
+
+
+def test_add_product_to_db(client):
+    product_data = {
         'name': 'Product Name',
         'price': 9.99,
         'type': 'food',
         'region': 'Europe'
     }
 
-
-def test_add_product_to_db(product_data):
     response = client.post('/products', json=product_data)
     assert response.status_code == 201
     assert response.json() == product_data
+
+
+def test_add_product_with_negative_price(client):
+    product_data = {
+        'name': 'Product Name',
+        'price': -9.99,
+        'type': 'food',
+        'region': 'Europe'
+    }
+
+    response = client.post('/products', json=product_data)
+    assert response.status_code == 422
+
+
+def test_add_product_with_empty_name(client):
+    product_data = {
+        'name': '',
+        'price': 9.99,
+        'type': 'food',
+        'region': 'Europe'
+    }
+
+    response = client.post('/products', json=product_data)
+    assert response.status_code == 422
+
+
+def test_add_product_with_invalid_type(client):
+    product_data = {
+        'name': 'Product Name',
+        'price': 9.99,
+        'type': 'invalid_type',
+        'region': 'Europe'
+    }
+
+    response = client.post('/products', json=product_data)
+    assert response.status_code == 422
